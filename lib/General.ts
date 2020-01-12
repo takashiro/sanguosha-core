4321bf53ca0a01847637428d7ab9fbdb6fb473b5
Kazuichi Takashiro <takashiro@qq.com>
2020-01-12 17:12:27 +0800

    Add class General

---
+++ lib/General.ts
@@ -0,0 +1,114 @@
+import Gender from './Gender';
+import Kingdom from './Kingdom';
+
+interface GeneralPofile {
+	kingdom: number;
+	name: string;
+}
+
+class General {
+	protected id: number;
+
+	protected name: string;
+
+	protected kingdom: Kingdom;
+
+	protected maxHp: number;
+
+	protected gender: Gender;
+
+	protected emperor: boolean;
+
+	protected hidden: boolean;
+
+	protected headExtraMaxHp: number;
+
+	protected deputyExtraMaxHp: number;
+
+	protected companions: Set<string>;
+
+	protected skills: object[];
+
+	constructor(name: string, kingdom: Kingdom, maxHp: number, gender: Gender) {
+		this.id = 0;
+		this.name = name;
+		this.kingdom = kingdom;
+		this.maxHp = maxHp;
+		this.gender = gender;
+
+		this.emperor = false;
+		this.hidden = false;
+
+		this.headExtraMaxHp = 0;
+		this.deputyExtraMaxHp = 0;
+
+		this.companions = new Set();
+
+		this.skills = [];
+	}
+
+	getId(): number {
+		return this.id;
+	}
+
+	getName(): string {
+		return this.name;
+	}
+
+	getKingdom(): Kingdom {
+		return this.kingdom;
+	}
+
+	getMaxHp(): number {
+		return this.maxHp;
+	}
+
+	getGender(): Gender {
+		return this.gender;
+	}
+
+	setEmperor(emperor: boolean): void {
+		this.emperor = emperor;
+	}
+
+	isEmperor(): boolean {
+		return this.emperor;
+	}
+
+	isHidden(): boolean {
+		return this.hidden;
+	}
+
+	getHeadMaxHp(): number {
+		return this.maxHp + this.headExtraMaxHp;
+	}
+
+	getDeputyMaxHp(): number {
+		return this.maxHp + this.deputyExtraMaxHp;
+	}
+
+	addCompanion(companion: General): void {
+		this.companions.add(companion.getName());
+	}
+
+	isCompanionWith(general: General): boolean {
+		return this.companions.has(general.getName());
+	}
+
+	addSkill(skillClass: object): void {
+		this.skills.push(skillClass);
+	}
+
+	getSkills(): object[] {
+		return this.skills;
+	}
+
+	toJSON(): GeneralPofile {
+		return {
+			kingdom: this.getKingdom(),
+			name: this.getName(),
+		};
+	}
+}
+
+export default General;
