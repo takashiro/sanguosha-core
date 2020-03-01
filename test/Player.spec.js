1f5bac2f7d8483d382933d8e171d483b8e0bde51
Kazuichi Takashiro <takashiro@qq.com>
2020-03-01 16:25:42 +0800

    Add tests of Player

---
+++ test/Player.spec.js
@@ -0,0 +1,30 @@
+import Player from '../lib/Player';
+
+describe('Player', () => {
+	const player = new Player();
+
+	it('sets/gets HP', () => {
+		player.setHp(4);
+		expect(player.getHp()).toBe(4);
+	});
+
+	it('sets/gets max HP', () => {
+		player.setMaxHp(4);
+		expect(player.getMaxHp()).toBe(4);
+	});
+
+	it('is not wounded', () => {
+		expect(player.isWounded()).toBe(false);
+	});
+
+	it('is alive', () => {
+		expect(player.isAlive()).toBe(true);
+		expect(player.isDead()).toBe(false);
+	});
+
+	it('is wounded', () => {
+		player.setHp(1);
+		expect(player.isAlive()).toBe(true);
+		expect(player.getLostHp()).toBe(3);
+	});
+});
