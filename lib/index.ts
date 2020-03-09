dd0fad6032a0156e2c51c7a34e4d989d81db9684
Kazuichi Takashiro <takashiro@qq.com>
2020-03-09 23:43:56 +0800

    Build with webpack

---
+++ lib/index.ts
@@ -0,0 +1,40 @@
+
+export {
+	default as Card,
+	Color as CardColor,
+	Suit as CardSuit,
+	Type as CardType,
+} from './Card';
+
+export {
+	default as CardArea,
+	Type as CardAreaType,
+	Direction as CardAreaDirection,
+} from './CardArea';
+
+export {
+	default as Player,
+	Phase as PlayerPhase,
+	Role as PlayerRole,
+} from './Player';
+
+export {
+	default as Skill,
+	Tag as SkillTag,
+	Type as SkillType,
+} from './Skill';
+
+export {
+	default as SkillArea,
+	Type as SkillAreaType,
+} from './SkillArea';
+
+export { default as Command } from './Command';
+
+export { default as DamageType } from './DamageType';
+
+export { default as Gender } from './Gender';
+
+export { default as General } from './General';
+
+export { default as Kingdom } from './Kingdom';
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = function (env, argv) {
+	const mode = (argv && argv.mode) || 'production';
+	return {
+		mode,
+		target: 'node',
+		entry: {
+			index: './lib/index.ts',
+		},
+		module: {
+			rules: [
+				{
+					test: /\.ts$/,
+					exclude: /node_modules/,
+					use: 'ts-loader',
+				},
+			],
+		},
+		resolve: {
+			extensions: [
+				'.ts',
+			],
+		},
+		output: {
+			filename: '[name].js',
+			path: path.join(__dirname, 'dist'),
+			libraryTarget: 'commonjs2',
+			devtoolModuleFilenameTemplate: '[absolute-resource-path]',
+		},
+		devtool: mode !== 'production' ? 'source-map' : undefined,
+	};
+};
